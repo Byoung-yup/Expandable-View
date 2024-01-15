543af27c8bc52f2cad0bae73251de19177ed52ef
ByoungYup <20170926@vision.hoseo.edu>
2024-01-15 13:28:45 +0900

    Initial Commit

---
+++ Expandable View/Expandable_ViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expandable_ViewApp.swift
+//  Expandable View
+//
+//  Created by 김병엽 on 2024/01/15.
+//
+
+import SwiftUI
+
+@main
+struct Expandable_ViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
